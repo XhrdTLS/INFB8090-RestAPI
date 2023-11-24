55b0cb5dd8ec6eb0d2357581acf96591993925ad
Tomás L <59000698+XhrdTLS@users.noreply.github.com>
2023-11-24 00:27:15 -0300

    24112023b pg to fl0
    - Se cambia la lógica al acceder a la base de datos, ahora alojada en fl0
    
    Co-Authored-By: Nicolás <50126580+kouzze@users.noreply.github.com>
    Co-Authored-By: Gustavo <87094184+gxstvo@users.noreply.github.com>

--- config/pg.js
+++ config/pg.js
@@ -1,12 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
+const sequelize = new Sequelize('postgres://postgres://fl0user:IH1XDliJZ8ae@ep-tight-morning-52314840.us-east-2.aws.neon.fl0.io:5432/database?sslmode=require') // Postges en FL0
+
+/*const sequelize = new Sequelize({
   dialect: 'postgres',
   host: 'localhost',
   database: 'postgres',
   username: 'admin',
   password: 'admin',
   port: 5432,
-});
+});*/
 
 module.exports = sequelize;
