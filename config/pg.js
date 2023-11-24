2a36180c215f91fe455f153b609e6c18766a16e1
Tomás L <59000698+XhrdTLS@users.noreply.github.com>
2023-11-24 00:48:47 -0300

    24112023c URL PSQL y callback
    - Se modifica el callback para que devuelva a la página de fl0 en vez de al localhost
    - Se cambia la bdd, por probar.
    
    Co-Authored-By: Nicolás <50126580+kouzze@users.noreply.github.com>
    Co-Authored-By: Gustavo <87094184+gxstvo@users.noreply.github.com>

--- config/pg.js
+++ config/pg.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres://postgres://fl0user:IH1XDliJZ8ae@ep-tight-morning-52314840.us-east-2.aws.neon.fl0.io:5432/database?sslmode=require') // Postges en FL0
+const sequelize = new Sequelize('postgres://fl0user:IH1XDliJZ8ae@ep-tight-morning-52314840.us-east-2.aws.neon.fl0.io:5432/database?sslmode=require') // Postges en FL0
 
 /*const sequelize = new Sequelize({
   dialect: 'postgres',
