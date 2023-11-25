c4449c69e37205d1e90485468e25d0b6f745e00c
Tomás L <59000698+XhrdTLS@users.noreply.github.com>
2023-11-24 22:40:11 -0300

    24112023h Conexión BDD fl0
    - Se cambia la estructura de la base de datos, ya que se utilizará en modo SSL
    
    Co-Authored-By: Gustavo <87094184+gxstvo@users.noreply.github.com>
    Co-Authored-By: Nicolás <50126580+kouzze@users.noreply.github.com>

--- config/pg.js
+++ config/pg.js
@@ -1,14 +1,18 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres://fl0user:IH1XDliJZ8ae@ep-tight-morning-52314840.us-east-2.aws.neon.fl0.io:5432/database?sslmode=require') // Postges en FL0
+//const sequelize = new Sequelize('postgres://fl0user:IH1XDliJZ8ae@ep-tight-morning-52314840.us-east-2.aws.neon.fl0.io:5432/database?sslmode=require') // Postges en FL0
 
-/*const sequelize = new Sequelize({
+const sequelize = new Sequelize({
   dialect: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  username: 'admin',
-  password: 'admin',
+  host: 'ep-tight-morning-52314840.us-east-2.aws.neon.fl0.io',
+  database: 'database',
+  username: 'fl0user',
+  password: 'IH1XDliJZ8ae',
   port: 5432,
-});*/
+  ssl: {
+    enabled: true,
+  }
+});
+
 
 module.exports = sequelize;
